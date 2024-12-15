a90550821d6c0c8bc564b935c83a4399ec6d4556
ryanlikesramen <ryanmsori@gmail.com>
2024-12-14 23:05:07 -0600

    added UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <p>my portfolio</p>
+      <Button>Click me</Button>
     </div>
   );
 }
